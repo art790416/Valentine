d1e452fc6c66c2efe7755a997564d1c7d312bda2
art790416 <art790416@hotmail.com>
2021-12-23 22:30:29 +0800

    shiyou
    edit

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-function scrollTo(e){
-    e.preventDefault();
-    var target = $(e.currentTarget).attr('href');
-    var offset = $(target).offset();
-    $('html, body').animate({scrollTop: offset.top});
-};
-
-$('.scroll').click(scrollTo);
